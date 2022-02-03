285003e28ddffa8738cf64a950728db27d063530
stavroskasidis <stavroskasidis@outlook.com>
2022-02-03 12:02:16 +0200

    added sample apps build script

---
+++ scripts/build-sample-apps.ps1
@@ -0,0 +1,36 @@
+Push-Location $PSScriptRoot
+
+function Write-Message{
+    param([string]$message)
+    Write-Host
+    Write-Host $message
+    Write-Host
+}
+function Confirm-PreviousCommand {
+    param([string]$errorMessage)
+    if ( $LASTEXITCODE -ne 0) { 
+        if( $errorMessage) {
+            Write-Message $errorMessage
+        }    
+        exit $LASTEXITCODE 
+    }
+}
+
+function Confirm-Process {
+    param ([System.Diagnostics.Process]$process,[string]$errorMessage)
+    $process.WaitForExit()
+    if($process.ExitCode -ne 0){
+        Write-Host $process.ExitCode
+        if( $errorMessage) {
+            Write-Message $errorMessage
+        }    
+        exit $process.ExitCode 
+    }
+}
+
+Write-Message "Building ..."
+dotnet publish ../sampleapps/BlazorHostedSampleApp/Server/BlazorHostedSampleApp.Server.csproj -c Release -o ../artifacts/sample-apps/BlazorHostedSampleApp
+Confirm-PreviousCommand
+
+
+Write-Message "Build completed successfully"
