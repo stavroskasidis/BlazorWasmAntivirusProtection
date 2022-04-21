c340e860c43d1c48b6111a1e87840a907f162a92
Galdin Raphael <gldraphael@users.noreply.github.com>
2022-04-21 23:56:48 +0530

    Minimal changes to repro behavior

--- sampleapps/BlazorHostedSampleApp/Server/Program.cs
+++ sampleapps/BlazorHostedSampleApp/Server/Program.cs
@@ -31,6 +31,6 @@ app.UseRouting();
 
 app.MapRazorPages();
 app.MapControllers();
-app.MapFallbackToFile("index.html");
+app.MapFallbackToFile("/app/{*path:nonfile}", "app/index.html");
 
 app.Run();
